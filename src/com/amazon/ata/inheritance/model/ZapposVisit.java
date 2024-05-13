99eafe2793ab2022d231c7f2b014ceee7df06308
Santiago Coria <santiago_coria@hotmail.com>
2024-05-13 19:43:10 -0300

    Visit Factory

---
+++ src/com/amazon/ata/inheritance/model/ZapposVisit.java
@@ -0,0 +1,14 @@
+package com.amazon.ata.inheritance.model;
+
+public class ZapposVisit extends Visit {
+
+    @Override
+    public char getVisitCharacter() {
+        return '@';
+    }
+
+    @Override
+    public VisitColor getVisitColor() {
+        return VisitColor.ZAPPOS_VISITOR_COLOR;
+    }
+}
